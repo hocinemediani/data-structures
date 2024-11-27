b86d2f1f2de6a6d2e84920fc4b08a126529dc7e8
hocinemediani <dolcejeannarciss@gmail.com>
2024-11-27 21:34:14 +0100

    git push

--- th_sujet.adb
+++ th_sujet.adb
@@ -16,40 +16,39 @@ procedure th_sujet is
    function "+" (Item : in String) return Unbounded_String
 		renames To_Unbounded_String;
 
+
    HashTable : hashMap;
-
-   Key1 : CONSTANT Unbounded_String := +"uo";
-   Key2 : CONSTANT Unbounded_String := +"cinq";
-   Key3 : CONSTANT Unbounded_String := +"de";
-   Key4 : CONSTANT Unbounded_String := +"bu";
-   Key5 : CONSTANT Unbounded_String := +"ap";
-   Key6 : CONSTANT Unbounded_String := +"apadadadadadadada";
-   Value1 : CONSTANT Integer := 42;
-   Value2 : CONSTANT Integer := 51;
-   Value3 : CONSTANT Integer := 51;
-   Value4 : CONSTANT Integer := 888;
-   Value5 : CONSTANT Integer := 45699;
-   Value6 : CONSTANT Integer := 1111;
-
    arrayLength : CONSTANT Integer := 11;
+   Keys : CONSTANT array (0 .. 6) of Unbounded_String
+      := (+"un", +"deux", +"trois", +"quatre", +"cinq", +"vingt-et-un", +"quatre-vingt-dix-neuf");
 
 begin
 
    InitialiseHashTable (HashTable, arrayLength);
 
-   Register (HashTable, Key1, Value1);
-   Register (HashTable, Key2, Value2);
-   Register (HashTable, Key3, Value3);
-   Register (HashTable, Key4, Value4);
-   Register (HashTable, Key5, Value5);
-   Register (HashTable, Key6, Value6);
+   Register (HashTable, Keys (0), 1);
+   Register (HashTable, +"ddddddddddd", 50);
+   Register (HashTable, +"ddddddddezd", 78);
+   Register (HashTable, +"dddddazdddd", 485);
+   Register (HashTable, Keys (1), 2);
+   Register (HashTable, Keys (2), 3);
+   Register (HashTable, Keys (3), 4);
+   Register (HashTable, Keys (4), 5);
+   Register (HashTable, Keys (5), 21);
+   Register (HashTable, Keys (6), 99);
 
    DisplayHashTable (HashTable);
 
    -- Problèmes à regler :
-      -- Les clefs ne sont pas bien enregistrés (pour une hashedKey données, toutes les entrées sont égales).
+      -- Les clefs ne sont pas bien enregistrés (pour une hashedKey donnée, toutes les entrées sont égales).
       -- Reprendre la fonction DestroyHashMap et vérifier avec valgrind la présence ou non de memory leaks.
       -- Reprendre les autres fonction en supprimant les redondances et en profitant de la structure des hashTables pour optimiser le code.
+
+   Delete (HashTable, +"cinq");
+   Delete (HashTable, +"vingt-et-un");
+   Delete (HashTable, +"trois");
+
+   DisplayHashTable (HashTable);
 
    DestroyHashTable (HashTable);
 
